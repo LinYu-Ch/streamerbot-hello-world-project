e80d1464957b00ed53fdf0480ddfdb2246c27c61
linyu-ch <linyuenhong@gmail.com>
2024-12-11 15:41:28 -0600

    working, testable environment

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
 import { StreamerbotClient } from '@streamerbot/client';
+const display = document.getElementById('displayWindow');
+
+/*  global message tracker
+    @TODO: important: global message count object
+    should not be used for production aimed handlers, use 
+    window context and visibility checks instead
+*/
+let globalMsgCount = 0;
+
+// initiating connection with streamerbot: see https://streamerbot.github.io/client/guide/events
+const client = new StreamerbotClient();
+
+// just a test request / response operation to check connections, 
+// not relevant to main program
+// const response = await client.getActions();
+// console.log(`get actions response: ${response}`);
+
+// event subscription event handler method
+// client.on();
+
+// creates an HTML element containing a chat message
+const htmlWrapper = (data) => {
+    let user = data.message;
+    let row = document.createElement("div");
+
+    row.dataset.userId = data.user.id;
+    row.dataset.messageId = user.msgId;
+    row.className = "row";
+
+    row.innerHTML = `<div>
+        <div class="message__container">
+            <div class="message__sender">${user.username}</div>
+            <div class="message__body">
+                <p class="message__content">${user.message}</p>
+            </div>
+        </div>
+    </div>`
+    return row;
+}
+
+// event handler for twitch chat messages
+client.on('Twitch.ChatMessage', (obj) => {
+    const data = obj.data;
+    const event = obj.event;
+
+    display.prepend(htmlWrapper(data));
+    console.log(obj);
+})
